87d532c9f4cb3008fd6eae2e3a185b8c113de88b
anas <anasazzouz8@gmail.com>
2023-05-18 19:37:51 +0100

    remove auth

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,9 @@ import { SessionProvider } from "next-auth/react";
 
 export default function App({Component, pageProps: { session, ...pageProps }}) {
   return (
-    <SessionProvider session={session}>
+    <SessionProvider 
+    session={session}
+    >
       <Component {...pageProps}/>
     </SessionProvider>
   )
