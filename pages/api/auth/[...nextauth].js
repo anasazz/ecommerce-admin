bce53dddddf01a4b38c90fa4357deeeafac4963c
anas <anasazzouz8@gmail.com>
2023-05-23 23:24:20 +0100

    test

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,7 +3,7 @@ import GoogleProvider from 'next-auth/providers/google'
 import {MongoDBAdapter} from "@next-auth/mongodb-adapter";
 import clientPromise from "@/lib/mongodb";
 
-const adminEmails = ['anasazzouz8@gmail.com'];
+const adminEmails = ['anasazzouz8@gmail.com','anasazzouz.0908@gmail.com'];
 
 export const authOptions = {
   secret: process.env.SECRET,
